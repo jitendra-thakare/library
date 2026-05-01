325909d3d7b83558b01443c7c29f36856f01fb0a
jitendra-thakare <jitendrathakare2000@gmail.com>
2026-05-01 18:07:46 +0530

    commit-10-delete-genre

--- src/main/java/com/booksrepo/library/service/impl/GenreServiceImpl.java
+++ src/main/java/com/booksrepo/library/service/impl/GenreServiceImpl.java
@@ -79,6 +79,7 @@ public class GenreServiceImpl implements GenreService {
 	public void hardDeleteGenre(Long genreId)  throws GenreException {
 		Genre existingGenre =  genreRepository.findById(genreId).orElseThrow(
 				()-> new GenreException("Genre not found."));
+		genreRepository.delete(existingGenre);
 		
 	}
 
