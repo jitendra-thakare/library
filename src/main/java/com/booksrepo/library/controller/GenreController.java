3035aadd5c9df1429afbacbf04e4bc986651603f
jitendra-thakare <jitendrathakare2000@gmail.com>
2026-05-01 18:53:11 +0530

    commit-12-deactivate-reactivate-delete-genre

--- src/main/java/com/booksrepo/library/controller/GenreController.java
+++ src/main/java/com/booksrepo/library/controller/GenreController.java
@@ -3,6 +3,7 @@ package com.booksrepo.library.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -17,6 +18,7 @@ import lombok.RequiredArgsConstructor;
 
 import com.booksrepo.library.exception.GenreException;
 import com.booksrepo.library.payload.dto.GenreDTO;
+import com.booksrepo.library.payload.response.ApiResponse;
 
 @RestController
 @RequiredArgsConstructor
@@ -46,5 +48,23 @@ public class GenreController {
 	public ResponseEntity<?> updateGenre(@RequestParam("genreId")Long genreId,@RequestBody GenreDTO genreDTO) throws GenreException{
 		GenreDTO genres = genreService.updateGenreDTO(genreId, genreDTO);
 		return ResponseEntity.ok(genres);
+	}
+	@DeleteMapping("{genreId}")
+	public ResponseEntity<?> deleteGenre(@RequestParam("genreId")Long genreId) throws GenreException{
+		genreService.deactiveGenre(genreId);
+		ApiResponse response = new ApiResponse("Genre deactivated successfully", true);
+		return ResponseEntity.ok(response);
+	}
+	@DeleteMapping("{genreId}/activate")
+	public ResponseEntity<?> recoverGenre(@RequestParam("genreId")Long genreId) throws GenreException{
+		genreService.reactiveGenre(genreId);
+		ApiResponse response = new ApiResponse("Genre reactivated successfully", true);
+		return ResponseEntity.ok(response);
+	}
+	@DeleteMapping("{genreId}/hard")
+	public ResponseEntity<?> hardDeleteGenre(@RequestParam("genreId")Long genreId) throws GenreException{
+		genreService.hardDeleteGenre(genreId);
+		ApiResponse response = new ApiResponse("Genre deleted successfully", true);
+		return ResponseEntity.ok(response);
 	}
 }
