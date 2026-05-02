9cd0a853825384a6c1c5c9ca678322aaea708d16
jitendra-thakare <jitendrathakare2000@gmail.com>
2026-05-02 20:34:12 +0530

    commit-17-book-service-interface

---
+++ src/main/java/com/booksrepo/library/service/BookService.java
@@ -0,0 +1,25 @@
+package com.booksrepo.library.service;
+
+import java.util.List;
+
+import javax.print.attribute.standard.PageRanges;
+
+import com.booksrepo.library.payload.dto.BookDTO;
+import com.booksrepo.library.payload.request.BookSearchRequest;
+import com.booksrepo.library.payload.response.PageResponse;
+
+public interface BookService {
+	BookDTO createBook(BookDTO bookDTO);
+	List<BookDTO> createBooksBulk(List<BookDTO> bookDTOList);
+	BookDTO getBookById(Long bookId);
+	BookDTO updateBook(Long bookId, BookDTO bookDTO);
+	void deactivateBook(Long bookId);
+	void reactivateBook(Long bookId);
+	void hardDeleteBook(Long bookId);
+	
+	PageResponse<BookDTO> searchBooksWithFilters(BookSearchRequest searchRequest);
+	Long getTotalActiveBooks();
+	Long getTotalAvailableBooks();
+	
+}
+
